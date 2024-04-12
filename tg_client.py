0b4fa0de5303fdbd1ea6cbbd05a8304a160a2d68
Vladislav Kalman <Vladislav99355@gmail.com>
2024-04-12 17:23:46 +0300

    temp commit for get comments

--- tg_client.py
+++ tg_client.py
@@ -42,23 +42,49 @@ async def get_channel_info(channel_name: str) -> dict:
 
 def save_messages(channel_name, messages: list):
     """Ротация по полученным сообщениям и их сохранение"""
+
+    print(f'Сохранение сообщений - {channel_name}')
     result_db = ResultDataBase()
 
     for message in messages:
-        print(f'Сохранение сообщений - {channel_name}')
+        print(f'{type(message)=}')
+        post = message["post"]
+        comments = message["comments"]
 
         try:
-            message_text = message.content.text.text
+            message_text = post.content.text.text
+            print(f'{message_text=}')
         except AttributeError:
-            message_text = None
+            try:
+                message_text = post.content.caption.text
+            except AttributeError:
+                print(f'AttributeError {post=}')
+                message_text = None
 
-        json_message = json.dumps(message.dict())
+        json_message = json.dumps(post.dict())
         base64_message = base64.b64encode(json_message.encode()).decode('utf-8')
 
         # сохранение результатов
         source_id = 2
         saved_message_id = result_db.save_result_post(message_text, base64_message, source_id)
 
+        if comments:
+            comments_for_save = list()
+
+            for comment in comments:
+                try:
+                    comment_text = comment.content.text.text
+                except AttributeError:
+                    continue
+
+                json_comment = json.dumps(comment.dict())
+                base64_comment = base64.b64encode(json_comment.encode()).decode('utf-8')
+
+                comments_for_save.append((saved_message_id, comment_text, base64_comment))
+
+            print(f'{post=}')
+            print(f'{comments_for_save=}')
+            result_db.save_result_comment(comments_for_save)
 
 async def get_messages(channel_name: str):
     """Получение и сохранение сообщений из канала/группы"""
@@ -73,10 +99,15 @@ async def get_messages(channel_name: str):
             last_message_id = chat.last_message.id
             last_message_item = chat.last_message
 
+            # print(f'{last_message_item.dict()=}')
+            # print(f'{last_message_item.can_get_message_thread=}')
+
             all_messages = list()
             all_messages.append(last_message_item)
+            message_with_comments = list()
 
             while True:
+                print(f'{len(all_messages)=}')
                 print(f'Получение сообщений - {channel_name} - {last_message_id=}')
                 try:
                     messages_history = await client.api.get_chat_history(chat_id=channel_id,
@@ -88,21 +119,77 @@ async def get_messages(channel_name: str):
 
                 if messages := messages_history.messages:
                     for message_in_chat in messages:
+
                         all_messages.append(message_in_chat)
+                    #     last_message_id = message_in_chat.id
+                    #
+                    #     print(f'{message_in_chat=}')
+
+
+                    for message_in_chat in all_messages:
+                        # получение комментариев к посту
+                        # print(f'base_post - {message_in_chat}')
+
+                        all_post_comments = list()
+                        if message_in_chat.can_get_message_thread is True:
+                            print(f'Получение комментариев - {channel_name} - {last_message_id=}')
+                            # last_comment = await client.api.get_message_thread(chat_id=chat.id,
+                            #                                                    message_id=message.id)
+                            # last_comment_id = last_comment.messages[0].id
+                            last_comment_id = 0
+
+                            while True:
+                                # print(f'Получение комментариев - {channel_name} - {message_in_chat.id=} - {last_comment_id=}')
+                                try:
+                                    comments_history = await client.api.get_message_thread_history(chat_id=chat.id,
+                                                                                           message_id=message_in_chat.id,
+                                                                                           from_message_id=last_comment_id,
+                                                                                           limit=100, offset=0                             ,
+                                                                                           request_timeout=30,
+                                                                                           skip_validation=True)
+
+                                    if comments_history:
+                                        # last_comment_id = comments_history.messages[0].id  # reply_message.id
+                                        for reply_comment in comments_history.messages:
+                                            # print(f'{reply_comment.dict()=}')
+
+                                            all_post_comments.append(reply_comment)
+                                            last_comment_id = reply_comment.id
+                                except api.errors.error.AioTDLibError as comment_error:
+                                    if comment_error.message == 'Receive messages in an unexpected chat':
+                                        # print(f'{comment_error=}')
+                                        break
+                                except asyncio.exceptions.TimeoutError:
+                                    print(f'get_chat_history - TimeoutError')
+                                    continue
+                                finally:
+                                    pass
+
+
+                        all_post_comments.reverse()
+                        to_save = {'post': message_in_chat,
+                                   'comments': all_post_comments
+                        }
+
+                        message_with_comments.append(to_save)
                         last_message_id = message_in_chat.id
 
-                    if all_messages:
-                        save_messages(channel_name, all_messages)
-                        all_messages.clear()
+                    if message_with_comments:
+                        save_messages(channel_name, message_with_comments)
+                        message_with_comments.clear()
+
+                    all_messages.clear()
 
                 else:
                     break
 
             return True
 
-        except api.errors.error.BadRequest as channel_error:
-            print(f'{channel_error.message=}')
-            return False
+        # except api.errors.error.BadRequest as channel_error:
+        #     print(f'{channel_error.message=}')
+        #     return False
+        finally:
+            pass
 
 
 async def rotate():
@@ -126,7 +213,7 @@ async def rotate():
             await get_messages(channel_name)
 
             # установка статуса завершения
-            db.set_channel_finish(channel_id)
+            # db.set_channel_finish(channel_id)
 
 
 if __name__ == '__main__':
